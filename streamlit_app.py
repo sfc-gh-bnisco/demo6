d5d2dedf6b89dfd37ef5d86c3719a393e89b7826
Bob Nisco <bob.nisco@snowflake.com>
2026-06-24 12:47:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Welcome to the Demo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is a sample Streamlit application running on Snowflake. Feel free to customize it however you like!"
 )
